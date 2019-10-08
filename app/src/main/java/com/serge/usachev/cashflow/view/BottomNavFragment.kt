0fe8e701e59aaeda6712668f204c59eeab847f98
sergeusachev <seergee1313@gmail.com>
2019-10-09 00:18:57 +0300

    Fix nabigation graphs

--- app/src/main/java/com/serge/usachev/cashflow/view/BottomNavFragment.kt
+++ app/src/main/java/com/serge/usachev/cashflow/view/BottomNavFragment.kt
@@ -5,8 +5,12 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
 import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.NavigationUI
 import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.serge.usachev.cashflow.R
 import kotlinx.android.synthetic.main.fragment_bottom_nav.*
 
@@ -19,7 +23,7 @@ class BottomNavFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val navController = findNavController()
+        val navController = Navigation.findNavController(requireActivity(), R.id.bottom_nav_host_fragment)
         main_bottom_navigation.setupWithNavController(navController)
     }
 }
